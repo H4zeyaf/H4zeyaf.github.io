9c3dbe73f31087373cde39c6fcb8f70251d71a50
H4zeyaf <gouy.hamza@gmail.com>
2026-02-01 09:06:06 +0100

    Lower loader logo slightly more on mobile

--- shader-loader.js
+++ shader-loader.js
@@ -158,7 +158,7 @@ class ShaderLoader {
         const width = vv ? vv.width : window.innerWidth;
         const offsetTop = vv ? vv.offsetTop : 0;
         const offsetLeft = vv ? vv.offsetLeft : 0;
-        const yOffset = width <= 768 ? Math.round(height * 0.04) : 0;
+        const yOffset = width <= 768 ? Math.round(height * 0.06) : 0;
         this.logoOverlay.style.top = `${offsetTop + height / 2 + yOffset}px`;
         this.logoOverlay.style.left = `${offsetLeft + width / 2}px`;
     }
